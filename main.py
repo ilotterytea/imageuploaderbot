d7e4171d57849d5c5dbcb0759cc959ca6695525c
ilotterytea <me@ilotterytea.kz>
2025-06-21 00:56:29 +0500

    feat: save uploaded file

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+from os.path import exists
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, CallbackContext, filters
+from os import mkdir
+
+async def download_file(update: Update, _: CallbackContext) -> None:
+    if not exists("./temp"):
+        mkdir("./temp")
+
+    if len(update.message.photo) > 0:
+        file_id = update.message.photo[-1]
+        file = await update.get_bot().get_file(file_id)
+        file_path = f"./temp/{file_id.file_unique_id}"
+        await file.download_to_drive(file_path)
+
+    await update.message.reply_text('Saved!')
+
 def run():
-    print("kill all niggers")
+    print("Starting the bot...")
+    app = ApplicationBuilder().token("xd").build()
+    app.add_handler(MessageHandler(filters.PHOTO, download_file))
+    app.run_polling()
 
 if __name__ == "__main__":
     run()
